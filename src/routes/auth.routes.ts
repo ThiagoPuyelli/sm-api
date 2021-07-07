158b20579d569d3cc81a51566d1b3a13d83528fd
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-07-07 17:00:51 -0300

    Ruta para buscar nombre y apellido

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,6 +4,16 @@ import jwt from 'jsonwebtoken'
 import sendResponse from '../utils/sendResponse'
 const router = Router()
 
+router.get('/me', passport.authenticate('token'), (req, res) => {
+  try {
+    const { name, lastname } = req.user
+
+    return sendResponse(res, 200, { name, lastname })
+  } catch (err) {
+    return sendResponse(res, 500, err.message || 'Server error')
+  }
+})
+
 router.post('/sign-in', passport.authenticate('login'), (req, res) => {
   try {
     const token = jwt.sign({ userID: req.user._id }, process.env.JWT_PASSWORD, {
